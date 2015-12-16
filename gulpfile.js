70bc5a5ce8c9381bfc2edf01a7874e711dfa19c5
Isabella Organ <isabellaorgan@gmail.com>
2015-12-16 10:34:22 -0800

    add dashboard, sass file structure, begin sass, update package.json and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 var gulp = require('gulp');
-
+var minifyCss = require('gulp-minify-css');
+var gulpWatch = require('gulp-watch');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
 
 gulp.task('static:dev', function() {
   gulp.src('app/**/*.html')
   .pipe(gulp.dest('build/'));
 });
 
+gulp.task('sass:dev', function() {
+  gulp.src('./app/sass/**/*.scss')
+  .pipe(maps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCss())
+  .pipe(maps.write('./'))
+  .pipe(gulp.dest('build/css'));
+});
 
-gulp.task('build', ['static:dev']);
+gulp.task('sass:watch', function () {
+  gulp.watch(['./app/sass/**/*.scss', './app/index.html'], ['sass:dev', 'static:dev']);
+});
+
+gulp.task('build', ['static:dev', 'sass:dev']);
+gulp.task('default', ['build:dev']);
