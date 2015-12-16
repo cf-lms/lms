e6bf38ae299bf92cd099400dbe1f31aa91bdd8fc
Isabella Organ <isabellaorgan@gmail.com>
2015-12-16 11:17:07 -0800

    add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
+	eslint = require('gulp-eslint');
 var minifyCss = require('gulp-minify-css');
 var gulpWatch = require('gulp-watch');
 var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
 
 gulp.task('static:dev', function() {
-  gulp.src('app/**/*.html')
+  return gulp.src('app/**/*.html')
   .pipe(gulp.dest('build/'));
 });
 
+gulp.task('lint', function () {
+	return gulp.src(['app/**/*.js', '!node_modules/**'])
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError());
+});
+ 
+
 gulp.task('sass:dev', function() {
-  gulp.src('./app/sass/**/*.scss')
+  return gulp.src('./app/sass/**/*.scss')
   .pipe(maps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(minifyCss())
@@ -24,3 +32,4 @@ gulp.task('sass:watch', function () {
 
 gulp.task('build', ['static:dev', 'sass:dev']);
 gulp.task('default', ['build:dev']);
+gulp.task('default', ['lint']);
