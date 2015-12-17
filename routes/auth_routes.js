4d07df5d67353c0ce29ed6ee775b444b30e1919c
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-12-17 11:33:22 -0800

    oauth: now with 60% less vanilla

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var https = require('https');
+var request = require('superagent');
+var handleErr = require(__dirname + '/../lib/handle_error');
+
 var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
 
@@ -10,40 +12,15 @@ authRouter.get('/', function(req, res) {
 });
 
 authRouter.get('/token', function(req, res) {
-  var options = {
-    host: 'github.com',
-    path: '/login/oauth/access_token?client_id=' +
-      clientId + '&client_secret=' + clientSecret + '&code=' +
-      req.query.code,
-    method: 'POST',
-    headers: {
-      Accept: 'application/json'
-    }
-  };
-  var postData = JSON.stringify({
-    client_id: clientId,
-    client_secret: clientSecret,
-    code: req.query.code
-  });
-  var sendOauth = https.request(options, function(authRes) {
-    var data = '';
-    authRes.setEncoding('utf8');
-    authRes.on('data', function(chunk) {
-      data += chunk;
+  request
+    .post(process.env.ROUTE || 'https://github.com/login/oauth/access_token')
+    .query({client_id: clientId})
+    .query({client_secret: clientSecret})
+    .query({code: req.query.code})
+    .end(function(err, data) {
+      if (err) return handleErr(err, res);
+      // Need to build user DB to save token encrypt and save token
+      res.json({token: data.body.access_token});
     });
-    authRes.on('end', function() {
-      req.oauth = JSON.parse(data);
-      // check for errors, store token and redirect
-
-      // for test purposes
-      res.json(req.oauth);
-    });
-  });
-
-  sendOauth.on('err', function(e) {
-    console.log('problems: ' + e.message);
-  });
-  sendOauth.write(postData);
-  sendOauth.end();
 });
 
--- test/auth_test.js
+++ test/auth_test.js
@@ -2,10 +2,20 @@ var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
 var expect = chai.expect;
+var testServer = require('express')();
 
+process.env.CLIENT_ID = '123';
+process.env.CLIENT_SECRET = 'abc';
+process.env.ROUTE = 'localhost:5000/test-token';
 require(__dirname + '/../server');
 
 describe('The auth routes', function() {
+  before(function() {
+    testServer.post('/test-token', function(req, res) {
+      res.json({access_token: 'success'});
+    });
+    testServer.listen(5000);
+  });
   it('should redirect', function(done) {
     chai.request('localhost:3000')
       .get('/auth')
@@ -20,7 +30,7 @@ describe('The auth routes', function() {
       .get('/auth/token?code=thisisnotarealcode')
       .end(function(err, res) {
         expect(err).to.eql(null);
-        expect(res.body.error).to.eql('bad_verification_code');
+        expect(res.body.token).to.eql('success');
         done();
       });
   });
