0126f26a1f915b19dd2ced6934a8062abe8eaf1c
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-12-17 12:46:51 -0800

    updated user model for token storage

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose');
+var eat = require('eat');
 
 var userSchema = new mongoose.Schema({
-  username: {type: String, require: true, index: true, unique: true},
-  admin: {type: Boolean, default: false}
+  username: {type: String, index: true, unique: true},
+  admin: {type: Boolean, default: false},
+  token: String
 });
 
+userSchema.methods.storeToken = function(token, cb) {
+  var id = this.id;
+
+  eat.encode({id: id}, process.env.APP_SECRET, cb);
+};
+
 module.exports = exports = mongoose.model('User', userSchema);
+
