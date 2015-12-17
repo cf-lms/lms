ef132f408d4f91c99aaab99157b9cd9c9dd9d091
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-12-17 13:52:25 -0800

    changes to auth routes, auth test, user model

--- models/user.js
+++ models/user.js
@@ -2,16 +2,13 @@ var mongoose = require('mongoose');
 var eat = require('eat');
 
 var userSchema = new mongoose.Schema({
-  username: {type: String, index: true, unique: true},
-  admin: {type: Boolean, default: false},
-  token: String
   username: {type: String, require: true, index: true, unique: true},
   admin: {type: Boolean, default: false},
   courseID: String,
   token: String
 });
 
-userSchema.methods.storeToken = function(token, cb) {
+userSchema.methods.storeToken = function(cb) {
   var id = this.id;
 
   eat.encode({id: id}, process.env.APP_SECRET, cb);
