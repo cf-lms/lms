87727ff1cff6e0f25066ba928520d1d4a86a8a40
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-12-16 16:00:45 -0800

    added oauth router

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+var authRouter = require(__dirname + '/routes/auth_routes');
 
 app.use('/', express.static(__dirname + '/build'));
+app.use('/auth', authRouter);
 
 app.use(function(req, res) {
   res.status(404).send('could not find file');
