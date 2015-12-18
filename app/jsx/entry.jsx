064c9084189f54b921cad657cafe9b1d29daec94
Isabella Organ <isabellaorgan@gmail.com>
2015-12-18 11:44:06 -0800

    adjust button structure, remove require sass from entry.jsx

--- app/jsx/entry.jsx
+++ app/jsx/entry.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-//require('./css/application.css');
 var AssignmentBox = require(__dirname + '/assignment_box/assignment_box.jsx');
 var HeaderBox = require(__dirname + '/header_box/header_box.jsx');
 var AsideBox = require(__dirname + '/aside_box/aside_box.jsx');
