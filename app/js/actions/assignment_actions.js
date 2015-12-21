fdb7d6d17629ad5a218904aa9dd55eaf24532d34
Ryan Heathers <ryan@ryanheathers.com>
2015-12-21 14:37:13 -0800

    fix merge conflicts

--- app/js/actions/assignment_actions.js
+++ app/js/actions/assignment_actions.js
@@ -1,45 +1,107 @@
 require('isomorphic-fetch');
 var types = require(__dirname + '/../constants/action_types');
 
-module.exports.requestAssignments = function() {
-  return {
-    type: REQUEST_ASSIGNMENTS
-  }
-}
 
-module.exports.fetchAssignments = function() {
-  return function(dispatch, getState) {
-    var state = getState();
-    var id = state.user._id;
+module.exports.fetchAssignments = function(callback, secondCallback) {
+  return function(dispatch) {
+    return fetch('http://localhost:3000/api/assignments')
+      .then(function(res) {
+        if (res.status >= 200 && res.status > 300) {
 
-    // tell state that fetch is starting
-    dispatch(requestAssignments);
-
-    return fetch('/api/assignments/' + id)
-      .then(function(result) {
-        if (result.status === 200) {
-          return result.json();
+          return res.json();
         }
         throw 'request failed';
       })
-      .then(function(jsonResult) {
-        dispatch(receiveAssignments(jsonResult));
+      .then(function(res) {
+        return dispatch(callback(res, secondCallback));
       })
       .catch(function(err) {
         console.log('unable to fetch assignments');
-      });
+    });
   };
-}
+};
 
-module.exports.receiveAssignments = function(json) {
-  return {
-    type: RECEIVE_ASSIGNMENTS,
-    items:[],
-    receivedAt: Date.now()
+module.exports.sortAssignments = function(assignments, callback) {
+
+  var upcoming = [];
+  var current = [];
+  var late = [];
+  var turnedIn = [];
+  var now = new Date();
+  var nextCoupleDays = new Date(now.getTime() + 15 * 24 * 60 * 60 * 1000);
+
+  for (var i = 0; i < assignments.length; i++) {
+    if(assignments[i].turnedIn === true) {
+      turnedIn.push(assignments[i]);
+      continue;
+    }
+
+    var dueDate = new Date(assignments[i].dueDate);
+
+    if(dueDate < now) {
+      late.push(assignments[i]);
+    }
+
+    if(dueDate > now && dueDate < nextCoupleDays) {
+      current.push(assignments[i]);
+    }
+
+    if(dueDate > nextCoupleDays) {
+      upcoming.push(assignments[i]);
+    }
   }
-}
 
-// TODO create action for AJAX error handling
+  return callback(upcoming, current, late, turnedIn);
+};
+
+module.exports.receiveAssignments = function(upcoming, current, late, turnedIn) {
+  return {
+    type: types.RECEIVE_ASSIGNMENTS,
+    upcoming: upcoming,
+    current: current,
+    late: late,
+    turnedIn: turnedIn
+  };
+};
+
+module.exports.handleSubmit = function(id, context, callback) {
+  return function(dispatch) {
+    return fetch('http://localhost:3000/api/assignments/' + id, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        turnedIn: true
+      })
+    })
+      .then(function(res) {
+        if (res.status <= 200 || res.status > 300) {
+          return dispatch(callback(id, context));
+        }
+        throw 'request failed';
+      })
+      .catch(function(err) {
+        console.log('unable to update assignments');
+    });
+  };
+};
+
+module.exports.updateAssignments = function(id, context) {
+  return {
+    type: types.UPDATE_ASSIGNMENTS,
+  _id: id,
+  context: context
+  };
+};
 
 
+module.exports.handleExpandClick = function(expand, context) {
+  return {
+    type: types.HANDLE_EXPAND_CLICK,
+    expand: !expand,
+    context: context
+  };
+};
 
