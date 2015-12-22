a126ff68d08556747c4150caffd37d3ac82ff132
Ryan Heathers <ryan@ryanheathers.com>
2015-12-21 18:36:14 -0800

    update layout

--- app/js/components/aside_box/aside_box.jsx
+++ app/js/components/aside_box/aside_box.jsx
@@ -15,9 +15,11 @@ module.exports = React.createClass({
   render: function() {
     return (
       <nav>
-        <a href="#"><img className="cadet-logo" src="images/cadet-logo.svg" alt="cadet logo" height="70" width="70"/></a>
-        <a href="#" className="icon-home" onClick={this.handleHomeClick}></a>
-        <a href="#" className="icon-cog" onClick={this.handleCourseCreatorClick}></a>
+        <ul>
+          <li><a href="#"><img className="cadet-logo" src="images/cadet-logo.svg" alt="cadet logo" height="70" width="70"/></a></li>
+          <li><a href="#" className="icon-home" onClick={this.handleHomeClick}></a></li>
+          <li><a href="#" className="icon-cog" onClick={this.handleCourseCreatorClick}></a></li>
+        </ul>
       </nav>
     );
   }
--- app/js/components/header_box/auth_button/auth_button.jsx
+++ app/js/components/header_box/auth_button/auth_button.jsx
@@ -5,7 +5,9 @@ var AuthButton = module.exports = React.createClass({
 
   render: function() {
     return(
-      <a onClick={this.props.handleAuthClick} href='https://github.com/login/oauth/authorize?client_id=1fe1ac6261a105f3ee49'>{this.props.loggedInStatus ? "Log Out" : "Log In"}</a>
+      <div className="login">
+        <a className="button" onClick={this.props.handleAuthClick} href='https://github.com/login/oauth/authorize?client_id=1fe1ac6261a105f3ee49'>{this.props.loggedInStatus ? "Log Out" : "Log In"}</a>
+      </div>
     );
   }
 });
