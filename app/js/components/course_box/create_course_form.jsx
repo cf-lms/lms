a475acb23d92825d33aa24cc733a7d121cc1f378
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-12-21 18:41:35 -0800

    removed commented code

--- app/js/components/course_box/create_course_form.jsx
+++ app/js/components/course_box/create_course_form.jsx
@@ -22,19 +22,17 @@ module.exports = React.createClass({
 
   render: function() {
     return (
-     // <form id="courseForm">
         <div id='courseForm'>
-        <label htmlFor="course-name">Course Name</label>
-        <input type="text" name="course-name" onChange={this.handleCourseName} />
+          <label htmlFor="course-name">Course Name</label>
+          <input type="text" name="course-name" onChange={this.handleCourseName} />
 
-        <label htmlFor="course-master-url">Link to Course Master Markdown File</label>
-        <input type="text" name="course-master-url" onChange={this.handleCourseUrl} />
+          <label htmlFor="course-master-url">Link to Course Master Markdown File</label>
+          <input type="text" name="course-master-url" onChange={this.handleCourseUrl} />
 
-        <label htmlFor="course-start-date">Start Date</label>
-        <input type="date" name="course-start-date" onChange={this.handleCourseStartDate} />
-        <button type="button" onClick={this.handleSubmit}>Create Course</button>
+          <label htmlFor="course-start-date">Start Date</label>
+          <input type="date" name="course-start-date" onChange={this.handleCourseStartDate} />
+          <button type="button" onClick={this.handleSubmit}>Create Course</button>
         </div>
-      //</form>
     );
   }
 });
